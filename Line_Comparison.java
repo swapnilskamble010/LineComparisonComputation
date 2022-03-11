fcafbade06986d9b5862b65d918ae76b6a140d62
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-11 14:45:45 +0530

    Welcome message added

---
+++ Line_Comparison.java
@@ -0,0 +1,10 @@
+package line_compare;
+
+public class Line_Comparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+	}
+
+}
