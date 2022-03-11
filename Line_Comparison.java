ff34687be6ab80ba18f6274e9c9beb44ff464881
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-11 17:35:27 +0530

    Length of lines are compared

--- Line_Comparison.java
+++ Line_Comparison.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 public class Line_Comparison {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program");
-		length1Calc();
 		
-
-	}
-	public static void length1Calc() {
+		System.out.println("Welcome to Line Comparison Computation Program");
+		
+		//caculating length of line 1
 		float x1,y1,x2,y2;
 		System.out.println("Please Enter the co-ordinates of 1st Line");
 		Scanner sc = new Scanner(System.in);
@@ -18,11 +16,30 @@ public class Line_Comparison {
 		y1 = sc.nextFloat();
 		x2 = sc.nextFloat();
 		y2 = sc.nextFloat();
-		sc.close();
 		double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-		System.out.println("Length of line:" + Math.round(length1*100.0)/100.0);
+		System.out.println("Length of line1 = " + Math.round(length1*100.0)/100.0);	
 		
+		//calculating length of line 2
+		float x3,y3,x4,y4;
+		System.out.println("Please Enter the co-ordinates of 2nd Line");
+		x3 = sc.nextFloat();
+		y3 = sc.nextFloat();
+		x4 = sc.nextFloat();
+		y4 = sc.nextFloat();
+		sc.close();
+		double length2 = Math.sqrt((x4-x3)*(x4-x3) + (y4-y3)*(y4-y3));
+		System.out.println("Length of line2 = " + Math.round(length2*100.0)/100.0);
 		
-		
+		//checking equality of lines
+		if (length1 == length2) {
+			System.out.println("Length of lines are equal");
+		}
+		else {
+			System.out.println("Length of lines are not equal");
+		}
+			
+
 	}
+	
+
 }
